533acd52a7a98b2010c320768bb849a46053504e
Peter Arnold <peter@entroprise.com>
2020-11-23 17:27:44 -0500

    asd

--- src/components/ClientComponent.js
+++ src/components/ClientComponent.js
@@ -5,12 +5,7 @@ import IframeResizer from "iframe-resizer-react"
 import { makeStyles } from "@material-ui/core/styles"
 
 const useStyles = makeStyles(theme => ({
-  frame: {
-    // zIndex: 2147483639,
-    // position: "fixed",
-    // top: 0,
-    // left: 0,
-  },
+  frame: {},
 }))
 const ClientComponent = () => {
   const classes = useStyles()
@@ -27,13 +22,14 @@ const ClientComponent = () => {
         inPageLinks
         frameBorder="none"
         src="https://entro-client-e7sfctcgkq-uc.a.run.app/"
-        src="http://localhost:3000/"
+        // src="http://localhost:3000/"
         style={{
           position: "fixed",
           width: "1px",
           top: "0px",
           right: "0px",
           minWidth: "100%",
+          maxHeight: "100%",
           zIndex: 2147483639,
         }}
       />
