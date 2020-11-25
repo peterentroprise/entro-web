918cab7e497b0e51322f4819d24eadc2849ab673
Peter Arnold <parnold@retrieve.com>
2020-11-25 11:06:41 -0500

    asd

--- functions/index.js
+++ functions/index.js
@@ -2,4 +2,15 @@ const functions = require("firebase-functions")
 
 exports.cloneNewUser = functions.auth.user().onCreate(user => {
   console.log(user)
+  db.collection("users")
+    .add({
+      displayName: user.displayName,
+      uid: user.uid,
+    })
+    .then(function (docRef) {
+      console.log("Document written with ID: ", docRef.id)
+    })
+    .catch(function (error) {
+      console.error("Error adding document: ", error)
+    })
 })
